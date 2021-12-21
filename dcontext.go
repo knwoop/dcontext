2fecc9996dbd2f111686f64fd6d0ad6c20fac6e7
kenta takahashi <kenta.takahashi@showcase-gig.com>
2021-12-21 10:32:30 +0900

    Add context detach func

---
+++ dcontext.go
@@ -0,0 +1,22 @@
+package dcontext
+
+import (
+	"context"
+	"time"
+)
+
+// Deadline detaches context from parent context
+func Detach(ctx context.Context) context.Context {
+	return dcontext{ctx}
+}
+
+type dcontext struct {
+	ctx context.Context
+}
+
+func (dcontext) Deadline() (time.Time, bool) { return time.Time{}, false }
+func (dcontext) Done() <-chan struct{}       { return nil }
+func (dcontext) Err() error                  { return nil }
+func (x dcontext) Value(key interface{}) interface{} {
+	return x.ctx.Value(key)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/knwoop/xcontext
+
+go 1.17
